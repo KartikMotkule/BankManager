c518e4fbe94e171fb61c2e2d29c9ab9fe7f5c46e
KartikMotkule <kartik2534@gmail.com>
2026-05-31 13:42:50 +0530

    Removed Town and Village from User Instance variable

--- src/main/java/com/Beans/User.java
+++ src/main/java/com/Beans/User.java
@@ -7,8 +7,7 @@ public class User {
     private String password;
     private String secQue;
     private String secAns;
-    private String Town;
-    private String Village;
+
 
     // Constructor
     public User(String userName, String password, String secQue, String secAns) {
